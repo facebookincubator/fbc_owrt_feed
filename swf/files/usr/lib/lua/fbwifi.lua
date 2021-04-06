1cf12f578c2bfb47d876e68d22e902e47468f2ad
Simon Kinane <skinane@fb.com>
2021-04-07 00:02:29 +0100

    Renames swf to fbwifi ; affects syslog, file names and paths, code prefixes, package name

--- swf/files/usr/lib/lua/fbwifi.lua
+++ swf/files/usr/lib/lua/fbwifi.lua
@@ -1,31 +1,31 @@
--- SWF Lua library
+-- FBWIFI Lua library
 -- function table
-local swf = {}
+local fbwifi = {}
 
 local http = require("ssl.https")
 local json = require("cjson")
 local log = require("posix.syslog")
 local uci = require("uci")
 
-function swf.gateway_token()
+function fbwifi.gateway_token()
 
 	state = uci.cursor(nil, "/var/state")
-	token = state:get("swf", "main", "gateway_token")
+	token = state:get("fbwifi", "main", "gateway_token")
 	if token and string.len(token) > 0 then
 		return token
 	else
-		log.syslog( log.LOG_WARNING, "[swf] UCI option swf.main.gateway_token is missing" )
+		log.syslog( log.LOG_WARNING, "[fbwifi] UCI option fbwifi.main.gateway_token is missing" )
 		return nil
 	end 
 end
 
-function swf.validate_token( token )
+function fbwifi.validate_token( token )
 
 	local valid = false
 
 	if string.len(token or '' ) > 0 then
 
-	        GATEWAY_TOKEN = swf.gateway_token()
+	        GATEWAY_TOKEN = fbwifi.gateway_token()
 
 	        URL="https://api.fbwifi.com/v2.0/token"
 	        BODY="token="..token
@@ -34,7 +34,7 @@ function swf.validate_token( token )
 	        if code==200 then
 	                valid = true
 	        else
-	                log.syslog(log.LOG_WARNING, "[swf] validate_token:"..body)
+	                log.syslog(log.LOG_WARNING, "[fbwifi] validate_token:"..body)
 	        end
 
 	end
@@ -42,9 +42,9 @@ function swf.validate_token( token )
 	return valid
 end
 
-function swf.instate_client_rule( token, client_mac )
+function fbwifi.instate_client_rule( token, client_mac )
 
-	log.syslog(log.LOG_INFO, "[swf] Validated client "..client_mac)
+	log.syslog(log.LOG_INFO, "[fbwifi] Validated client "..client_mac)
 
 	state = uci.cursor(nil, "/var/state")
 	state_name = "token_" .. token
@@ -53,10 +53,10 @@ function swf.instate_client_rule( token, client_mac )
 	RULE_FMT="iptables -w -t mangle -%s CLIENT_TO_INTERNET -m mac --mac-source \"%s\" -j MARK --set-mark 0xfb"
 	local RULE
 	
-	state:set("swf", state_name, "client")
-	state:set("swf", state_name, "token", token)
-	state:set("swf", state_name, "mac", client_mac)
-	state:set("swf", state_name, "authenticated", "true")
+	state:set("fbwifi", state_name, "client")
+	state:set("fbwifi", state_name, "token", token)
+	state:set("fbwifi", state_name, "mac", client_mac)
+	state:set("fbwifi", state_name, "authenticated", "true")
 				
 	-- verify a rule exists for the given client MAC, 
 	--   OR install it
@@ -64,26 +64,26 @@ function swf.instate_client_rule( token, client_mac )
 
 	res = os.execute(RULE)
 	if res ~= 0 then 
-		log.syslog(log.LOG_WARNING, string.format( "[swf] Failed to update iptables (%s)", res ) )
+		log.syslog(log.LOG_WARNING, string.format( "[fbwifi] Failed to update iptables (%s)", res ) )
 	end
-	log.syslog(log.LOG_INFO, "[swf] "..RULE)
+	log.syslog(log.LOG_INFO, "[fbwifi] "..RULE)
 	
-	state:save('swf')
+	state:save('fbwifi')
 end
 
-function swf.revoke_client_rule( token )
+function fbwifi.revoke_client_rule( token )
         
         if (token == nil) then
-                log.syslog(log.LOG_INFO, "[swf] Invalidating token, but token is Nil")
+                log.syslog(log.LOG_INFO, "[fbwifi] Invalidating token, but token is Nil")
                 return
         end
 
-	log.syslog(log.LOG_INFO, string.format( "[swf] Invalidating token (%s)", token) )
+	log.syslog(log.LOG_INFO, string.format( "[fbwifi] Invalidating token (%s)", token) )
 
 	state = uci.cursor(nil, "/var/state")
 	state_name = "token_" .. token
 	
-	client_mac = state:get("swf", state_name, "mac")
+	client_mac = state:get("fbwifi", state_name, "mac")
 
 	if client_mac then
 		RULE_COND="iptables -w -L CLIENT_TO_INTERNET -t mangle | grep -i -q \"%s\""
@@ -95,30 +95,30 @@ function swf.revoke_client_rule( token )
 
 		res = os.execute(RULE)
 		if res ~= 0 then 
-			log.syslog(log.LOG_WARNING, string.format( "[swf] Failed to update iptables (%s)", res ) )
+			log.syslog(log.LOG_WARNING, string.format( "[fbwifi] Failed to update iptables (%s)", res ) )
 		end
-		log.syslog(log.LOG_INFO, "[swf] "..RULE)
+		log.syslog(log.LOG_INFO, "[fbwifi] "..RULE)
 
-		state:delete("swf", state_name)
-		state:save('swf')
+		state:delete("fbwifi", state_name)
+		state:save('fbwifi')
 	else
-		log.syslog(log.LOG_WARNING, string.format( "[swf] Client MAC not found in DB (%s)", state_name ) )
+		log.syslog(log.LOG_WARNING, string.format( "[fbwifi] Client MAC not found in DB (%s)", state_name ) )
 	end
 end
 
-function swf.reset()
+function fbwifi.reset()
 
 	local success = false
-        GATEWAY_TOKEN = swf.gateway_token()
+        GATEWAY_TOKEN = fbwifi.gateway_token()
         URL="https://api.fbwifi.com/v2.0/gateway/reset"
 	BODY="{}"
         body, code, headers = http.request(URL.."?access_token="..GATEWAY_TOKEN, BODY)
 
         if code==200 then
-                log.syslog(log.LOG_INFO, "[swf] Reset committed")
+                log.syslog(log.LOG_INFO, "[fbwifi] Reset committed")
                 success = true
         else
-                log.syslog(log.LOG_WARNING, "[swf] Reset failed : "..body)
+                log.syslog(log.LOG_WARNING, "[fbwifi] Reset failed : "..body)
         end
 
 	return success
@@ -127,4 +127,4 @@ end
 --
 -- Return the function table to the host script
 --
-return swf
+return fbwifi
--- swf/files/usr/lib/lua/luci/controller/fbwifi.lua
+++ swf/files/usr/lib/lua/luci/controller/fbwifi.lua
@@ -1,12 +1,12 @@
 -- Copyright 
 -- Licensed to the public under the GNU General Public License v2.
 
-module("luci.controller.swf", package.seeall)
+module("luci.controller.fbwifi", package.seeall)
 
 sys = require "luci.sys"
 ut = require "luci.util"
 
 function index()
-    entry({"admin", "network", "swf"}, template("swf"), "Social Wi-Fi", 90).dependent=false
+    entry({"admin", "network", "fbwifi"}, template("fbwifi"), "Social Wi-Fi", 90).dependent=false
 end
 
